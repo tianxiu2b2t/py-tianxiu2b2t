c98a322279bdaafe75862427fa167f5f090dae49
tianxiu2b2t <atianxiua@163.com>
2025-03-08 01:21:37 +0800

    chore: 构建自动化 push

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ setup(
     name='tianxiu2b2t-utils',
     version='0.1.0',
     description='A collection of utilities for the tianxiu2b2t packages',
-    url='https://github.com/tianxiu2b2t/tianxiu2b2t-pyutils',
+    url='https://github.com/tianxiu2b2t/py-tianxiu2b2t',
     author='tianxiu2b2t',
     author_email='administrator@ttb-network.top',
     license='MIT',
     packages=[
         'tianxiu2b2t/anyio'
     ],
-    zip_safe=False
 )
