b9398152b8c47841658562d7a385454e6b55a241
tianxiu2b2t <atianxiua@163.com>
2025-04-15 23:58:50 +0800

    chore: 更新依赖

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_namespace_packages
 
+def read_requirements():
+    with open('requirements.txt') as f:
+        return [
+            line.strip()
+            for line in f.readlines()
+            if not line in (
+                'setuptools'
+            )
+        ]
+
 setup(
     name='tianxiu2b2t-utils',
-    version='0.1.29',
+    version='0.1.30',
     description='A collection of utilities for the tianxiu2b2t packages',
     url='https://github.com/tianxiu2b2t/py-tianxiu2b2t',
     author='tianxiu2b2t',
     author_email='administrator@ttb-network.top',
     license='MIT',
     packages=find_namespace_packages(),
-    install_requires=[
-        'anyio==4.9.0',
-        'h11==0.14.0',
-        'h2==4.2.0',
-        'hpack==4.1.0',
-        'hyperframe==6.1.0',
-        'idna==3.10',
-        'sniffio==1.3.1',
-        'typing_extensions==4.13.2',
-        'wsproto==1.2.0'
-    ],
+    install_requires=read_requirements()
 )
