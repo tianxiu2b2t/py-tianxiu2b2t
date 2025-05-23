5ffd585df7e886a910495dd999c46935a5c48c4e
tianxiu2b2t <atianxiua@163.com>
2025-05-23 20:29:57 +0800

    feat: 加了个 ConfigParser

--- src/tianxiu2b2t/parser.py
+++ src/tianxiu2b2t/parser.py
@@ -1,7 +1,11 @@
+import json
+from typing import Any
+
+
 class ConfigParser:
     def __init__(
         self,
-        value: str
+        value: Any
     ):
         self._required = False
         self._value = value
@@ -13,15 +17,27 @@ class ConfigParser:
     def not_required(self):
         self._required = False
         return self
-    
+
     def as_int(self):
+        self._check_required()
         return int(self._value)
     
     def as_float(self):
+        self._check_required()
         return float(self._value)
     
     def as_bool(self):
+        self._check_required()
         return bool(self._value)
     
     def as_str(self):
+        self._check_required()
         return str(self._value)
+    
+    def as_json(self):
+        self._check_required()
+        return json.loads(self._value)
+    
+    def _check_required(self):
+        if self._value is None and self._required:
+            raise ValueError("Value is required")
